1563419563ceec98037399a4bcd4ade0dd711adf
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-12-07 13:25:46 +0100

    Add propTypes example

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
-const Header = () => {
+import PropTypes from "prop-types";
+
+const Header = (props) => {
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{props.title}</h1>
     </header>
   );
+};
+
+Header.defaultProps = {
+  title: "Task tracker",
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
 };
 
 export default Header;
