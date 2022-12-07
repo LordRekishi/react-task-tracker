bba11c99a89611578ee86f03ca7646db8a6f48d3
Patrik Fallqvist Magnusson <patrik.fallqvist@gmail.com>
2022-12-07 16:08:21 +0100

    Add button onClick

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = (props) => {
+  const onClick = () => {
+    console.log("Header click");
+  };
+
   return (
-    <header>
-      <h1 style={headingStyle}>{props.title}</h1>
+    <header className="header">
+      <h1>{props.title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
@@ -14,11 +20,6 @@ Header.defaultProps = {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-};
-
-const headingStyle = {
-  color: "red",
-  backgroundColor: "black",
 };
 
 export default Header;
